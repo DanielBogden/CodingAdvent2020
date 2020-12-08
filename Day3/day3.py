44929c0c1e268606fd9d74109089d64b2b115385
Daniel Bogden <danielbogden@u.boisestate.edu>
2020-12-08 11:43:00 -0700

    Day3 'cleaned up'

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,13 +1,10 @@
 import numpy
 
-run_totals = []
-
 # part 1 arguments
 # slope = [[3, 1]]
 
 # part 2 arguments
 slope = [[1,1], [3,1], [5,1], [7,1], [1,2]]
-
 
 def get_data():
     with open("input.txt", "r") as f:
@@ -16,24 +13,21 @@ def get_data():
 
 
 def Sonny_Bono(slope = []):
-    right_move = 0
-    down_move = 1
     ski_slope = get_data()
-    down = slope[0][down_move]
-    right = slope[0][right_move]
+    down = slope[0][1]
+    right = slope[0][0]
     num_trees = 0
 
     for _ in range (0, len(ski_slope) - 1, down):
         num_trees +=1 if ski_slope[down][right] == '#' else 0
 
-        right += slope[0][right_move]
-        down += slope[0][down_move]
+        right += slope[0][0]
+        down += slope[0][1]
         
         if (right >= len(ski_slope[0])):
             right = (right - (len(ski_slope[0])))
 
     return num_trees
-
 
 # Don't hit these trees!
 print(numpy.prod([Sonny_Bono([x]) for x in slope]))
